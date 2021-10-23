e1b15c19d6ea65f2d0931034fe3d2f8787a05076
Your Name <you@example.com>
2021-10-23 20:34:25 +0600

    'main'

---
+++ src/pages/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      net
+    </div>
+</template>
+
+<script>
+  import {defineComponent} from 'vue'
+    export default defineComponent ({
+        name: "Main",
+
+    })
+</script>
+
+<style scoped>
+
+</style>
