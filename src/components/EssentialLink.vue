4f8df86f1dada9056041d6d297ac38709b3edad6
Nursultan944 <85289213+Nursultan944@users.noreply.github.com>
2021-10-23 23:02:17 +0600

    lorem10

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et eum explicabo libero sint sunt veritatis voluptates! Accusamus ad amet aut beatae consequatur cumque deleniti earum ex expedita harum hic, illo itaque iure maiores minima minus nam neque nesciunt numquam pariatur praesentium quas saepe sequi suscipit tenetur ut voluptas voluptate. Accusantium beatae commodi deserunt dicta earum esse expedita, explicabo fugit hic impedit ipsa iste iusto, modi nam necessitatibus nihil nobis pariatur quaerat quas quasi quis ratione recusandae saepe sequi sit temporibus ut voluptatem? Accusamus aliquam amet at atque deleniti dicta dolore dolorem ea eius eligendi eos eum eveniet harum impedit in incidunt ipsa labore laborum magni molestiae, necessitatibus nisi, nobis nulla omnis quaerat quasi quibusdam, quidem sapiente sed tempora unde vel veniam voluptatum. Ad adipisci autem blanditiis commodi deserunt doloribus, et eum expedita id illum impedit ipsa iste itaque magnam necessitatibus nemo nihil officiis optio voluptas voluptate. Asperiores excepturi fuga illo.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, esse.
     </h1>
   </div>
   <q-item
