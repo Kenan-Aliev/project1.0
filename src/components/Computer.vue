f2462bb1424ee8f88978556b06ec03dc36e7901b
kmgliya <kuramaevagaliya28@gmail.com>
2021-11-02 21:25:54 +0600

    nurik

--- src/components/Computer.vue
+++ src/components/Computer.vue
@@ -1,35 +1,74 @@
 <template>
-    <div class="header">
-<div class="header__title">
-  <h1>Мощные игровые компьютеры</h1>
-  <span>Крутые компы для геймеров и киберспортсменов. Мощные ПК 2021 года, надежные компоненты, брендовое и самое быстрое компьютерное железо. Качественная графика GEFORCE RTX, запредельный FPS, идеальный геймплей. Системные блоки с повышенной производительностью - лучшие ПК для игр!</span>
-</div>
-      <div class="block">
-        <h1>Продвинутая серия - игровые компьютеры</h1>
+  <div class="header">
+    <div class="header__title">
+      <h1>Мощные игровые компьютеры</h1>
+      <span>Крутые компы для геймеров и киберспортсменов. Мощные ПК 2021 года, надежные компоненты, брендовое и самое быстрое компьютерное железо. Качественная графика GEFORCE RTX, запредельный FPS, идеальный геймплей. Системные блоки с повышенной производительностью - лучшие ПК для игр!</span>
+    </div>
+    <div class="block">
+      <h1>Продвинутая серия - игровые компьютеры</h1>
+    </div>
+    <div class="list">
+      <div class="list__pages">
+        <div class="list__pages__title">
+          <p>Мощные системные блоки, высокая скорость работы в играх и графических программах</p>
+        </div>
+        <div class="list__pages__title">
+          <p>Возможность играть на максимальных настройках графики, в проекты AAA</p>
+        </div>
+        <div class="list__pages__title">
+          <p>Качественные компаненты, классный дизайн, крутые игровые ПК EDELWEISS</p>
+        </div>
       </div>
-      <div class="list">
-        <div class="list__pages">
-          <div class="list__pages__title">
-              <p>Мощные системные блоки, высокая скорость работы в играх и графических программах</p>
+      <img src="../assets/robot.png" class="list__robot" alt="">
+    </div>
+  </div>
+
+  <div class="section">
+
+    <section class="section_plan">
+      <div class="section_list">
+
+        <img src="../assets/edel.png" alt="" class="freetariffimg">
+          <p class="section_name">EDELWEISS CS:GO</p>
+          <div class="selection_price" >
+            <p>156 360</p>
+            <q-spinner-puff color="purple" class="q-gutter-md row justify-center" style="font-size: 5em" />
           </div>
-          <div class="list__pages__title">
-              <p>Возможность играть на максимальных настройках графики, в проекты AAA</p>
+
+        <div class="table">
+          <div class="sectiontable">
+            <!--            <img src="images/jam_check.svg" alt="">-->
+            <span>Unlimited Bandwitch</span>
           </div>
-          <div class="list__pages__title">
-              <p>Качественные компаненты, классный дизайн, крутые игровые ПК EDELWEISS</p>
+          <div class="sectiontable">
+            <!--            <img src="images/jam_check.svg" alt="">-->
+            <span>Encrypted Connection</span>
+          </div>
+          <div class="sectiontable">
+            <!--            <img src="images/jam_check.svg" alt="">-->
+            <span>No Traffic Logs</span>
+          </div>
+          <div class="sectiontable">
+            <!--            <img src="images/jam_check.svg" alt="">-->
+            <span>Works on All Devices</span>
           </div>
         </div>
-        <img src="../assets/robot.png" class="list__robot" alt="">
+
+        <p style="font-size: 25px" class="sectionTitle">Free</p>
+        <div class="sectionbutton" style="font-weight: bold">Select</div>
       </div>
-    </div>
+
+    </section>
+
+  </div>
 
 
 </template>
 
 <script>
-    export default {
-        name: "Computer"
-    }
+  export default {
+    name: "Computer"
+  }
 </script>
 
 <style scoped>
